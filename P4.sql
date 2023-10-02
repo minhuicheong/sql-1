0dbb607a54d10badc5a19471abf99ac95bfa5a1e
minhuicheong <52492718+minhuicheong@users.noreply.github.com>
2023-10-02 16:20:29 +0800

    Add files via upload

---
+++ P4.sql
@@ -0,0 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 02, 2023 at 10:13 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+UPDATE inventories
+SET price = 120
+WHERE product_id = 1;
